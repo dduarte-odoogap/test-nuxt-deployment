3dbd5fd4b6cccc542ff2273caf46d32b9c4aca65
Diogo Duarte <dduarte@odoogap.com>
2024-02-29 22:40:23 +0000

    Chage config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false }
+  devtools: { enabled: false },
+  runtimeConfig: {
+    // Variables here are server-side only by default and won't be exposed to the client
+    redisHost: process.env.REDIS_HOST,
+    redisPort: process.env.REDIS_PORT,
+    redisPassword: process.env.REDIS_PASSWORD,
+    redisUser: process.env.REDIS_USER, // Optional, depending on your Redis setup.
+    // If you have variables that need to be available on both client and server, you can add them under public
+    public: {
+      // Example: apiBaseUrl: process.env.API_BASE_URL,
+    },
+  }
 })
--- server/api/redis-test.ts
+++ server/api/redis-test.ts
@@ -1,16 +1,23 @@
-// server/redis-test.ts
+// server/api/redis-test.ts
 import { defineEventHandler } from 'h3';
 import Redis from 'ioredis';
 
-const redis = new Redis({
-  host: 'test-nuxt-9urcxn.serverless.euc1.cache.amazonaws.com', // or your Redis server host
-  port: 6379, // default Redis port
-  // password: 'yourpassword', if your Redis instance is secured
-});
-
 export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig();
+  console.log(':::::::::::::::::::::::: ' + config.redisHost);
+  console.dir(config);
+
+  const redis = new Redis({
+    host: config.redisHost as string,
+    port: +config.redisPort as number,
+    password: config.redisPassword as string,
+    username: config.redisUser as string,
+    tls: {}
+  });
+
   const key = 'counter';
   const newCounterValue = await redis.incr(key);
+
   return new Response(JSON.stringify({ counter: newCounterValue }), {
     headers: {
       'Content-Type': 'application/json',
