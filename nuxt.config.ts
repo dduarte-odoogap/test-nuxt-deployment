d91afcb8a380f428669ffe2b4e2faf41c3fb373a
Diogo Duarte <dduarte@odoogap.com>
2024-02-29 18:18:05 +0000

    Change deploy to digital ocean

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false },
-  nitro: {
-    awsAmplify: {
-      // catchAllStaticFallback: true,
-      // imageOptimization: { "/_image", cacheControl: "public, max-age=3600, immutable" },
-      // imageSettings: { ... },
-    }
-  }
+  devtools: { enabled: false }
 })
